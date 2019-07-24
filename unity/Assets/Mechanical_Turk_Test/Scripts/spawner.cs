6d7a25cd5794c6bca015196e8eed7518a2f8c320
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-07-24 13:05:13 -0700

    Added functionality that can spawn in objects and take screenshots of them

---
+++ unity/Assets/Mechanical_Turk_Test/Scripts/spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    public GameObject[] comparables;
+    
+
+    public Transform spawnPos;
+    public GameObject spawnee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject a = spawnee;
+
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+            ScreenCapture.CaptureScreenshot("Assets/Mechanical_Turk_Test/Screenshots/A_" + a.name + "_B_" + ".jpg", 2);
+        }
+    }
+}
+
+
+//C:/Users/Eli VanderBilt/Documents/GitHub/ai2thor/unity/
